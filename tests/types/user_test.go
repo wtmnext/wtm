0026e4a77837b03b073fcfae026dcff4d0f24024
Nordine Bittich <contact@bittich.be>
2024-11-09 23:37:21 +0100

    rename

--- tests/types/user_test.go
+++ tests/types/user_test.go
@@ -19,10 +19,10 @@ func TestIsAvailable(t *testing.T) {
 		{
 			label: "Monday November 4th, 23:00h->05:00h next day",
 			availability: types.UserNormalAvailability{
-				Days:       []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
-				MinHour:    22,
-				MaxHour:    6,
-				HourPerDay: 8,
+				Days:        []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
+				MinHour:     22,
+				MaxHour:     6,
+				HoursPerDay: 8,
 			},
 
 			start:       time.Date(2024, time.November, 4, 23, 0, 0, 0, time.Now().Location()),
@@ -34,10 +34,10 @@ func TestIsAvailable(t *testing.T) {
 		{
 			label: "Monday November 4th, 06:00h->14:00h",
 			availability: types.UserNormalAvailability{
-				Days:       []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
-				MinHour:    6,
-				MaxHour:    22,
-				HourPerDay: 8,
+				Days:        []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
+				MinHour:     6,
+				MaxHour:     22,
+				HoursPerDay: 8,
 			},
 
 			start:       time.Date(2024, time.November, 4, 6, 0, 0, 0, time.Now().Location()),
@@ -48,10 +48,10 @@ func TestIsAvailable(t *testing.T) {
 		{
 			label: "Monday November 4th, 14:00h->22:00h",
 			availability: types.UserNormalAvailability{
-				Days:       []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
-				MinHour:    6,
-				MaxHour:    22,
-				HourPerDay: 8,
+				Days:        []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
+				MinHour:     6,
+				MaxHour:     22,
+				HoursPerDay: 8,
 			},
 
 			start:       time.Date(2024, time.November, 4, 14, 0, 0, 0, time.Now().Location()),
@@ -62,10 +62,10 @@ func TestIsAvailable(t *testing.T) {
 		{
 			label: "Monday November 4th, 22:00h->04:00h the next day",
 			availability: types.UserNormalAvailability{
-				Days:       []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
-				MinHour:    22,
-				MaxHour:    6,
-				HourPerDay: 8,
+				Days:        []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
+				MinHour:     22,
+				MaxHour:     6,
+				HoursPerDay: 8,
 			},
 
 			start:       time.Date(2024, time.November, 4, 22, 0, 0, 0, time.Now().Location()),
@@ -77,10 +77,10 @@ func TestIsAvailable(t *testing.T) {
 		{
 			label: "Monday November 4th, 22:00h->07:00h the next day (9h)",
 			availability: types.UserNormalAvailability{
-				Days:       []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
-				MinHour:    22,
-				MaxHour:    6,
-				HourPerDay: 8,
+				Days:        []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
+				MinHour:     22,
+				MaxHour:     6,
+				HoursPerDay: 8,
 			},
 
 			start:       time.Date(2024, time.November, 4, 22, 0, 0, 0, time.Now().Location()),
@@ -91,10 +91,10 @@ func TestIsAvailable(t *testing.T) {
 		{
 			label: "Monday November 4th, 22:00h->04:00h the next day but minHour 6 and maxHour 8",
 			availability: types.UserNormalAvailability{
-				Days:       []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
-				MinHour:    6,
-				MaxHour:    8,
-				HourPerDay: 8,
+				Days:        []time.Weekday{time.Monday, time.Tuesday, time.Wednesday, time.Tuesday, time.Thursday, time.Friday, time.Saturday},
+				MinHour:     6,
+				MaxHour:     8,
+				HoursPerDay: 8,
 			},
 
 			start:       time.Date(2024, time.November, 4, 22, 0, 0, 0, time.Now().Location()),
